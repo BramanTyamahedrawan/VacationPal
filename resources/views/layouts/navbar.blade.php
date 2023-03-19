7e875bfc48715e91632b44b742f93e534a5c08f6
bram <brammahendrawan693@gmail.com>
2023-03-19 21:32:43 +0700

    mengembalikan login seperti semula
    Signed-off-by: bram <brammahendrawan693@gmail.com>

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,11 +1,17 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand ml-auto" href="/">VacationPal</a>
+    <a class="navbar-brand" href="/">VacationPal</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="/">Home</a>
+            </li>
+        </ul>
+
         <ul class="navbar-nav ml-auto mr-5">
             <li class="nav-item">
                 <a class="nav-link" href="/pesan-tiket">Pesan Tiket</a>
