e418c95e13f86120ba623e68b27ced9f6b294f89
bram <brammahendrawan693@gmail.com>
2023-03-19 21:08:49 +0700

    mengganti databse
    Signed-off-by: bram <brammahendrawan693@gmail.com>

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,23 @@
 @extends('layouts.app')
-@section('title', 'Tiket Wisata Pantai 3 Warna')
 
 @section('content')
-    <h1>Tiket Wisata Pantai 3 Warna</h1>
-    <p>Pantai 3 Warna merupakan salah satu destinasi wisata yang terkenal di Indonesia. Terletak di daerah Malang, pantai ini memiliki keindahan yang memukau dengan tiga warna air laut yang berbeda di satu tempat. Kita dapat menikmati keindahan laut dan pasir putihnya dengan membeli tiket wisata.</p>
-    <p>Tiket wisata pantai 3 warna dapat dibeli secara online melalui situs ini atau dapat langsung dibeli di lokasi. Harga tiket yang kami tawarkan sangat bersahabat dengan kantong pelajar maupun mahasiswa. Jadi, tunggu apa lagi? Segera pesan tiket wisata pantai 3 warna untuk mengisi liburanmu bersama keluarga dan teman-temanmu!</p>
-    <a href="/pesan-tiket" class="btn btn-primary">Pesan Tiket</a>
-@endsection
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
 
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
