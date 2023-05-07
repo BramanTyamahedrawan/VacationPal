71500f97422988307540d5968d97df95ec04de41
EqiBaihaqi <baihaqi08.aa@gmail.com>
2023-05-08 00:47:07 +0700

    memperbaiki tampilan NavBar

--- resources/views/layouts_user/navbar_user.blade.php
+++ resources/views/layouts_user/navbar_user.blade.php
@@ -1,14 +1,14 @@
 <div class="container-fluid fixed-top px-0 wow fadeIn" data-wow-delay="0.1s">
-    <div class="top-bar row gx-0 align-items-center d-none d-lg-flex">
+    <div class="top-bar row gx-0 align-items-center d-none d-lg-flex bg-light">
         <div class="col-lg-6 px-5 text-start">
-            <small class="dark link-light"><i class="fa fa-map-marker-alt text-secondary me-2"></i>Pantai 3 Warna
+            <small class="dark link-dark"><i class="fa fa-map-marker-alt text-secondary me-2"></i>Pantai 3 Warna
                 Malang</small>
-            <small class="ms-4 dark link-light"><i class="fa fa-clock text-secondary me-2"></i>07.00 - 16.00</small>
+            <small class="ms-4 dark link-dark"><i class="fa fa-clock text-secondary me-2"></i>07.00 - 16.00</small>
         </div>
     </div>
-    <nav class="navbar navbar-expand-lg navbar-light py-lg-0 px-lg-5 wow fadeIn" data-wow-delay="0.1s">
+    <nav class="navbar navbar-expand-lg navbar-light py-lg-0 px-lg-5 wow fadeIn bg-white" data-wow-delay="0.1s">
         <a href="#" onclick="location.reload()" class="navbar-brand ms-4 ms-lg-0">
-            <h1 class="display-5 text-primary m-0 rounded" style="background: rgba(255, 255, 255, 0.44)">VacationPal
+            <h1 class="display-5 text-primary m-0 rounded">VacationPal
             </h1>
         </a>
         <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
