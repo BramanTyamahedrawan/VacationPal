d1ce81882ec546397407b4b0499e0916940dc793
Maisho20 <bahajazz70@gmail.com>
2023-05-11 13:18:11 +0700

    Update input number

--- resources/views/layouts_tiket/head.blade.php
+++ resources/views/layouts_tiket/head.blade.php
@@ -36,8 +36,13 @@
         html::-webkit-scrollbar-thumb {
             background: #2d2e37;
         }
+
+        input::-webkit-outer-spin-button,
+        input::-webkit-inner-spin-button {
+            -webkit-appearance: none;
+            margin: 0;
+        }
     </style>
 </head>
 
 <body>
-    
