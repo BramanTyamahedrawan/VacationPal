666214d9c78ef5ce50093e844810b4b7a14f760d
bram <brammahendrawan693@gmail.com>
2023-05-27 21:34:35 +0700

    admin bisa menghapus tiket yang dipesan berbagai user
    Signed-off-by: bram <brammahendrawan693@gmail.com>

--- app/Http/Controllers/BatalAdminController.php
+++ app/Http/Controllers/BatalAdminController.php
@@ -3,11 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User_tiket;
 
 class BatalAdminController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view('admin_views.batal_admin');
+        // Ambil semua data tiket
+        $tiket = User_tiket::find($request->tiket_id);
+
+        if ($tiket) {
+            // Ubah status tiket menjadi "Dibatalkan"
+            $tiket->status = 'Dibatalkan';
+            $tiket->save();
+        } else {
+            // Tangani ketika tiket tidak ditemukan
+            return view('admin_views.batal_admin', [
+                'tiketDibatalkan' => User_tiket::where('status', 'Dibatalkan')
+                    ->get()
+            ]);
+        }
+
+        // Pindahkan tiket yang dibatalkan ke halaman batal.blade.php
+        $tiketDibatalkan = User_tiket::where('status', 'Dibatalkan')
+            ->get();
+
+        return view('admin_views.batal_admin', ['tiketDibatalkan' => $tiketDibatalkan]);
     }
 }
