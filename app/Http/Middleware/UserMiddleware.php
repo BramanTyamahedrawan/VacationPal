f98a8814157199688f599e8a762206c4ede7e6ab
bram <brammahendrawan693@gmail.com>
2023-06-04 02:24:45 +0700

    menambahkan middleware user
    Signed-off-by: bram <brammahendrawan693@gmail.com>

---
+++ app/Http/Middleware/UserMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if ($request->user()->role !== 'user') {
+            abort(403, 'Admin tidak memiliki hak akses');
+
+            return $next($request);
+        }
+    }
+}
